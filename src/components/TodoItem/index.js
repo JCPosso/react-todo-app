0cb475a3e88110845cb8c010cf9d11446a1b486e
JCPosso <juancamiloposso01@gmail.com>
2023-06-12 17:12:37 -0500

    CREATE: Todo icon components and update TODO items

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
-
-function TodoItem() {
+import {CompleteIcon}  from '../TodoIcon/CompleteIcon'
+import {DeleteIcon} from '../TodoIcon/DeleteIcon'
+import './TodoItem.css';
+function TodoItem(props) {
   return (
-    <div>TodoItem</div>
-  )
+    <li className="TodoItem">
+      <CompleteIcon
+        completed={props.completed}
+        onComplete={props.onComplete}
+      />
+
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+        {props.text}
+      </p>
+
+      <DeleteIcon
+        onDelete={props.onDelete}
+      />
+    </li>
+  );
 }
 
-export default TodoItem
+export {TodoItem}
